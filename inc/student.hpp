3ee604b900973fb93d43fa0e12d9e321cf1d5e17
mryanstein <jan.habdas@gmail.com>
2026-06-22 01:02:14 +0200

    Zapisywanie podczas pracy

--- inc/student.hpp
+++ inc/student.hpp
@@ -7,7 +7,7 @@ class Student {
 private: 
     std::string imie;
     int ocena;
-    // TODO 1a: Dodaj prywatne pole 'int wiek'
+    int wiek;
 
 protected:
     int punktyKredytowe;
@@ -19,19 +19,16 @@ public:
     std::string pobierzImie() const;
     int pobierzOcene() const;
     void ustawOcene(int nowaOcena);
-
-    // TODO 1b: Zadeklaruj publiczny getter i setter dla wieku:
-    // int pobierzWiek() const;
-    // void ustawWiek(int nowyWiek);
-
+    int pobierzWiek() const;
+    void ustawWiek(int nowyWiek);
     virtual void wyswietlInfo() const;
 };
 
 class StudentInformatyki : public Student {
-// TODO 2a: Dodaj prywatne pole 'std::string ulubionyJezyk'
+private:
+    std::string ulubionyJezyk;
 public:
-    // TODO 2b: Zmień konstruktor tak, aby przyjmował trzeci parametr: 'std::string p_jezyk'
-    StudentInformatyki(std::string p_imie, int p_ocena);
+    StudentInformatyki(std::string p_imie, int p_ocena, std::string p_jezyk);
 
     void wyswietlInfo() const override;
 };
